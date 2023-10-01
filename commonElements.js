0eb1eac253ed9a251233922279ab70b0bc3375fa
IliyaZafirov <133087050+IliyaZafirov@users.noreply.github.com>
2023-10-01 13:22:21 +0300

    Initial Commit

---
+++ commonElements.js
@@ -0,0 +1,22 @@
+// 2. Common Elements
+// Write a function, which prints common elements in two string arrays. Print all matches on separate lines. Compare
+// the first array with the second array.
+
+
+
+
+function common(arr1, arr2) {
+
+    for (let elsArr1 of arr1) {
+
+        for (let elsArr2 of arr2) {
+
+            if (elsArr1 === elsArr2) {
+                console.log(elsArr1);
+            }
+        }
+    }
+
+}
+
+common(['Hey', 'hello', 2, 4, 'Peter', 'e'], ['Petar', 10, 'hey', 4, 'hello', '2'])
