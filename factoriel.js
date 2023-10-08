a41d04a4367c8533cfc079b29f322869166e9328
IliyaZafirov <133087050+IliyaZafirov@users.noreply.github.com>
2023-10-08 09:31:16 +0300

    100/100

---
+++ factoriel.js
@@ -0,0 +1,26 @@
+// Write a function that receives two integer numbers.
+// Calculate the factorial of each number.
+// Divide the first result by the second and print the division formatted to the second decimal point.
+
+function factorialDivision(num1, num2) {
+
+    let factorial1 = calcFactoriel(num1);
+    let factorial2 = calcFactoriel(num2);
+
+    let result = factorial1 / factorial2;
+
+    console.log(result.toFixed(2));
+
+    function calcFactoriel(num) {
+
+        let factorial = 1
+        while (num > 1) {
+            factorial *= num;
+            num--;
+        }
+        return factorial
+    }
+
+}
+
+factorialDivision(5, 2)
---
+++ passwordValidator.js
@@ -0,0 +1,63 @@
+// Password Validator
+// Write a function that checks if a given password is valid. Password validations are:
+// The length should be 6 - 10 characters (inclusive)
+// It should consist only of letters and digits
+// It should have at least 2 digits 
+// If a password is a valid print: "Password is valid".
+// If it is NOT valid, for every unfulfilled rule print a message:
+// "Password must be between 6 and 10 characters"
+// "Password must consist only of letters and digits"
+// "Password must have at least 2 digits"
+
+function validator(password) {
+    if (lengthValidator(password) && isLetterDigit(password) && numCountMinimum(password)) {
+        console.log('Password is valid');
+    } else {
+        if (!lengthValidator(password)) {
+            console.log('Password must be between 6 and 10 characters')
+        }
+        if (!isLetterDigit(password)) {
+            console.log('Password must consist only of letters and digits');
+        }
+        if (!numCountMinimum(password)) {
+            console.log(`Password must have at least 2 digits`);
+        }
+    }
+
+ 
+    function numCountMinimum(password) {
+        let count = 0;
+        // let isDigit = (x) => x >= 48 && x <= 57;
+
+        for (let char of password) {
+            let charValue = char.charCodeAt(0);
+            if (isDigit(charValue)) {
+                count++;
+            }
+        }
+
+        return count >= 2;
+    }
+
+    function lengthValidator(password) {
+        return password.length >= 6 && password.length <= 10;
+    }
+
+    function isDigit(num) {
+        return num >= 48 && num <= 57;
+    }
+
+    function isLetterDigit(password) {
+        let isLowerLetter = x => x >= 97 && x <= 122;
+        let isUpperLetter = x => x >= 65 && x <= 90;
+
+        for (let char of password) {
+            let charValue = char.charCodeAt(0);
+            if (!isDigit(charValue) && !isLowerLetter(charValue) && !isUpperLetter(charValue)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
