00344924f605a47b7ef59e71b7f92dbabd30c33b
IliyaZafirov <133087050+IliyaZafirov@users.noreply.github.com>
2023-10-11 08:27:23 +0300

    100/100

--- functions-lab/06-sign-check.js
+++ functions-lab/06-sign-check.js
@@ -1,13 +1,11 @@
 // Sign Check
 // Write a function, that checks whether the result of the multiplication numOne * numTwo * numThree is positive
-// or negative. Try to do this WITHOUT multiplying the 3 numbers.
+// or negative. 
 // Input
 // The input comes as parameters named numOne, numTwo, numThree.
 // Output
 // • If the result is positive, print on the console -> "Positive"
 // • Otherwise, print -> "Negative"
-
-
 
 function solve(numOne, numTwo, numThree) {
 
@@ -15,8 +13,5 @@ let signCheck = (x, y, z) => x * y * z > 0 ? 'Positive' : 'Negative';
 
 console.log(signCheck(numOne, numTwo ,numThree));
 
-
 }
-
-
 solve( 5, 12, -15)
