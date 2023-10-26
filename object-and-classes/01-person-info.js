65bd3ece8f8ca7134b5ea423116177fc24c9e5b1
IliyaZafirov <133087050+IliyaZafirov@users.noreply.github.com>
2023-10-26 09:25:49 +0300

    100/100

--- object-and-classes/01-person-info.js
+++ object-and-classes/01-person-info.js
@@ -1,3 +1,8 @@
+// Person Info
+// Write a function that receives 3 parameters, sets them to an object, and returns that object.
+// The input comes as 3 separate strings in the following order: firstName, lastName, age
+
+
 function personInfo(firstName, lastName, age) {
 
 
