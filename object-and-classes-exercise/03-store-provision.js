a45244e1085248edd365d84c84e3db8a26c571b5
IliyaZafirov <133087050+IliyaZafirov@users.noreply.github.com>
2023-10-28 10:46:22 +0300

    100/100

--- object-and-classes-exercise/03-store-provision.js
+++ object-and-classes-exercise/03-store-provision.js
@@ -1,41 +1,55 @@
-function storeProvision(store, delivery) {
-    let result = [];
-    class Store {
-      constructor(productName, quantity) {
-        this.productName = productName;
-        this.quantity = quantity;
-      }
-    }
-    for (let i = 0; i < store.length; i++) {
-      if (i % 2 !== 0) {
-        store[i] = +store[i];
-      }
-    }
-    for (let i = 0; i < delivery.length; i += 2) {
-      if (store.includes(delivery[i])) {
-        let index = store.indexOf(delivery[i]);
-        store[index + 1] += +delivery[i + 1];
-      } else {
-        store.push(delivery[i]);
-        store.push(delivery[i + 1]);
-      }
-    }
-  
-    for (let i = 0; i < store.length; i += 2) {
-      p = new Store(store[i], store[i + 1]);
-      result.push(p);
-    }
-  
-    for (const key of result) {
-      console.log(`${key.productName} -> ${key.quantity}`);
-    }
+// Store Provision
+// You will receive two arrays. The first array represents the current stock of the local store. The second array will
+// contain products that the store has ordered for delivery.
+// The following information applies to both arrays:
+// Every even index will hold the name of the product and every odd index will hold the quantity of that product.
+// The second array could contain products that are already in the local store. If that happens increase the quantity for
+// the given product. You should store them into an object, and print them in the following format: (product ->
+// quantity)
+// All of the arrays’ values will be strings.
+
+
+function storeProvision(stock, orderedProducts) {
+
+  let products = {};
+
+  for (let i = 0; i < stock.length; i += 2) {
+    let productName = stock[i];
+    let qty = +stock[i + 1];
+
+    products[productName] = qty;
   }
+
+  for (let i = 0; i < orderedProducts.length; i += 2) {
+    let productName = orderedProducts[i];
+    let qty = +orderedProducts[i + 1];
+
+    if (products.hasOwnProperty(productName)) {
+      products[productName] += qty;
+      continue;
+    }
+    products[productName] = qty;
+
+    // if (productName in products) {
+    //   products[productName] += qty;
+    // } else {
+    //   products[productName] = qty;
+    // }
+
+  }
+  // console.log(Object.entries(products)); 
+
+  for (let entry of Object.entries(products)) {
+    console.log(entry.join(' -> '));
+  }
+
+}
 storeProvision([
-    'Chips', '5', 'CocaCola', '9', 'Bananas',
-    '14', 'Pasta', '4', 'Beer', '2'
+  'Chips', '5', 'CocaCola', '9', 'Bananas',
+  '14', 'Pasta', '4', 'Beer', '2'
 ],
-    [
-        'Flour', '44', 'Oil', '12', 'Pasta', '7',
-        'Tomatoes', '70', 'Bananas', '30'
-    ]
+  [
+    'Flour', '44', 'Oil', '12', 'Pasta', '7',
+    'Tomatoes', '70', 'Bananas', '30'
+  ]
 )
