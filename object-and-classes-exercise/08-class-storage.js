865ad7c32eb650967cc0045293b4f1efb435075d
IliyaZafirov <133087050+IliyaZafirov@users.noreply.github.com>
2023-10-30 08:40:45 +0200

    100/100

--- object-and-classes-exercise/08-class-storage.js
+++ object-and-classes-exercise/08-class-storage.js
@@ -19,12 +19,11 @@
         constructor(capacity){
             this.capacity = capacity;
             this.storage = [];
-
             this.totalCost = 0;
 
         }
         addProduct(product) {
-            this.storage.push(product)
+            this.storage.push(product);
             this.capacity -= product.quantity;
             this.totalCost += product.price * product.quantity;
         }
