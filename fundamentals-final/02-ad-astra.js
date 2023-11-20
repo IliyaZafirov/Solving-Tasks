a16d21e68f5840210472a3ad23b28842f794effe
IliyaZafirov <133087050+IliyaZafirov@users.noreply.github.com>
2023-11-20 13:18:35 +0200

    100/100

---
+++ fundamentals-final/02-ad-astra.js
@@ -0,0 +1,62 @@
+// Problem 2 - Ad Astra
+// The problem for exam preparation for the Programming Fundamentals Course @SoftUni.
+// Submit your solutions in the SoftUni judge system at https://judge.softuni.org/Contests/Practice/Index/2525#1.
+
+// You are an astronaut who just embarked on a mission across the solar system. Since you will be in space for a long time, you have packed a lot of food with you. Create a program, which helps you identify how much food you have left and gives you information about its expiration date.
+// On the first line of the input, you will be given a text string. You must extract the information about the food and calculate the total calories. 
+// First, you must extract the food info. It will always follow the same pattern rules:
+// It will be surrounded by "|" or "#" (only one of the two) in the following pattern: 
+// #{item name}#{expiration date}#{calories}#   or 
+// |{item name}|{expiration date}|{calories}|
+// The item name will contain only lowercase and uppercase letters and whitespace.
+// The expiration date will always follow the pattern: "{day}/{month}/{year}", where the day, month, and year will be exactly two digits long.
+// The calories will be an integer between 0-10000.
+// Calculate the total calories of all food items and then determine how many days you can last with the food you have. Keep in mind that you need 2000kcal a day.
+// Input / Constraints
+// You will receive a single string.
+// Output
+// First, print the number of days you will be able to last with the food you have:
+// "You have food to last you for: {days} days!"
+// The output for each food item should look like this:
+// "Item: {item name}, Best before: {expiration date}, Nutrition: {calories}"
+
+function solve(input) {
+
+    let str = input.shift();
+
+    let pattern = /([|#])(?<name>[A-Za-z\s]+)\1(?<expirationDate>\d{2}\/\d{2}\/\d{2})\1(?<cals>[0-9]+)\1/g;
+
+    let valid = pattern.exec(str);
+    let foodItems = [];
+    let totalCalories = 0;
+
+    while (valid !== null) {
+
+        let itemName = valid.groups['name'];
+        let expirationDate = valid.groups['expirationDate'];
+        let calories = +valid.groups['cals'];
+
+        totalCalories += calories;
+
+        foodItems.push({
+            itemName,
+            expirationDate,
+            calories
+        });
+
+
+        valid = pattern.exec(str);
+    }
+
+    let days = Math.floor(totalCalories / 2000);
+
+    console.log(`You have food to last you for: ${days} days!`);
+
+    for (let item of foodItems) {
+        console.log(`Item: ${item.itemName}, Best before: ${item.expirationDate}, Nutrition: ${item.calories}`);
+    }
+
+}
+solve(['#Bread#19/03/21#4000#|Invalid|03/03.20||Apples|08/10/20|200||Carrots|06/08/20|500||Not right|6.8.20|5|'])
+solve(['$$#@@%^&#Fish#24/12/20#8500#|#Incorrect#19.03.20#450|$5*(@!#Ice Cream#03/10/21#9000#^#@aswe|Milk|05/09/20|2000|'])
+solve(['Hello|#Invalid food#19/03/20#450|$5*(@'])
