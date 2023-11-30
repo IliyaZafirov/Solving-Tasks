cb769bfe75728202a6060f6d824e655eadf7188f
IliyaZafirov <133087050+IliyaZafirov@users.noreply.github.com>
2023-11-30 20:59:26 +0200

    100/100

--- fundamentals-prep-final/03-the-pianist.js
+++ fundamentals-prep-final/03-the-pianist.js
@@ -28,7 +28,6 @@
 // Then you will receive multiple commands in the way described above until the command "Stop".
 // Output
 // All the output messages with the appropriate formats are described in the problem description.
-
 function solve(input) {
 
     let n = input.shift();
@@ -36,52 +35,66 @@ function solve(input) {
 
     for (let i = 0; i < n; i++) {
         let [piece, composer, key] = input[i].split('|');
-        obj[piece] = [composer, key];
+        obj[piece] = { composer, key };
     }
 
     for (let i = n; i < input.length; i++) {
         if (input[i] == 'Stop') break;
 
-        if (input[i].includes('Add')) {
-            let [command, piece, composer, key] = input[i].split('|');
+        if (input[i].includes('Add|')) {
+            let [, piece, composer, key] = input[i].split('|');
             if (!obj.hasOwnProperty(piece)) {
-                obj[piece] = [composer, key];
+                obj[piece] = { composer, key };
                 console.log(`${piece} by ${composer} in ${key} added to the collection!`);
             } else {
                 console.log(`${piece} is already in the collection!`);
             }
-        } else if (input[i].includes('Remove')) {
-            let [command, piece] = input[i].split('|');
+
+        } else if (input[i].includes('Remove|')) {
+            let [, piece] = input[i].split('|');
             if (obj.hasOwnProperty(piece)) {
                 delete obj[piece];
                 console.log(`Successfully removed ${piece}!`);
             } else {
                 console.log(`Invalid operation! ${piece} does not exist in the collection.`);
             }
-        } else if (input[i].includes('ChangeKey')) {
-            let [command, piece, newKey] = input[i].split('|');
+
+        } else if (input[i].includes('ChangeKey|')) {
+            let [, piece, newKey] = input[i].split('|');
             if (obj.hasOwnProperty(piece)) {
-                obj[piece][1] = newKey;
+                obj[piece].key = newKey;
                 console.log(`Changed the key of ${piece} to ${newKey}!`);
             } else {
                 console.log(`Invalid operation! ${piece} does not exist in the collection.`);
             }
         }
     }
-    
     for (let property in obj) {
-        console.log(`${property} -> Composer: ${obj[property][0]}, Key: ${obj[property][1]}`);
+        console.log(`${property} -> Composer: ${obj[property].composer}, Key: ${obj[property].key}`);
     }
 }
+// solve([
+//     '3',
+//     'Fur Elise|Beethoven|A Minor',
+//     'Moonlight Sonata|Beethoven|C# Minor',
+//     'Clair de Lune|Debussy|C# Minor',
+//     'Add|Sonata No.2|Chopin|B Minor',
+//     'Add|Hungarian Rhapsody No.2|Liszt|C# Minor',
+//     'Add|Fur Elise|Beethoven|C# Minor',
+//     'Remove|Clair de Lune',
+//     'ChangeKey|Moonlight Sonata|C# Major',
+//     'Stop'
+// ])
 solve([
-    '3',
-    'Fur Elise|Beethoven|A Minor',
-    'Moonlight Sonata|Beethoven|C# Minor',
-    'Clair de Lune|Debussy|C# Minor',
-    'Add|Sonata No.2|Chopin|B Minor',
-    'Add|Hungarian Rhapsody No.2|Liszt|C# Minor',
-    'Add|Fur Elise|Beethoven|C# Minor',
-    'Remove|Clair de Lune',
-    'ChangeKey|Moonlight Sonata|C# Major',
+    '4',
+    'Eine kleine Nachtmusik|Mozart|G Major',
+    'La Campanella|Liszt|G# Minor',
+    'The Marriage of Figaro|Mozart|G Major',
+    'Hungarian Dance No.5|Brahms|G Minor',
+    'Add|Spring|Vivaldi|E Major',
+    'Remove|The Marriage of Figaro',
+    'Remove|Turkish March',
+    'ChangeKey|Spring|C Major',
+    'Add|Nocturne|Chopin|C# Minor',
     'Stop'
-])
+  ])
